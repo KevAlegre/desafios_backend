d22140462018cd5725d892886a24323bd2cf3d56
KevAlegre <kovinalegre@gmail.com>
2024-04-15 21:52:28 -0300

    Desafío 3 - Server con Express

---
+++ Desafio-ProductManager-3/src/app.js
@@ -0,0 +1,50 @@
+//Desafío 3 - Servidor con Express
+
+const ProductManager = require("./ProductManager.js");
+
+const express = require("express");
+const app = express();
+const PORT = 8080;
+app.use(express.urlencoded({extended: true}));
+
+app.get("/products", async (req, res) => {
+    const limit = req.query.limit;
+    if(!isNaN(limit)) {
+        const products = await productManager.getProducts();
+        const limitedProducts = products.filter((prod) => prod.id <= limit);
+        return res.send({products: limitedProducts});
+    };
+
+    const products = await productManager.getProducts();
+    res.send({products});
+});
+
+app.get("/products/:pid", async (req, res) => {
+    const productId = parseInt(req.params.pid);
+    const product = await productManager.getProductById(productId)
+    res.send({product});
+    
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+
+const productManager = new ProductManager();
+
+//Agregar productos
+
+// productManager.addProduct("Harina", "0000", 870, "Sin imagen", "asd123", 12);
+// productManager.addProduct("Arroz", "Yamaní", 530, "Sin imagen", "jhb528", 15);
+// productManager.addProduct("Jugo", "Limonada", 125, "Sin imagen", "bpu528", 18);
+
+
+//Llamar elementos
+
+//Para el método de update se utilizan tres parámetros (id, propiedad-a-cambiar, valor)
+
+// productManager.updateProduct(1, "stock", 10);
+
+// productManager.deleteProduct();
