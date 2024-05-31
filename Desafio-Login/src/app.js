1c9c27e7073297bf185ee70c93cb12491731baf0
KevAlegre <kovinalegre@gmail.com>
2024-05-30 23:55:24 -0300

    Implementación de login

--- Desafio-Login/src/app.js
+++ Desafio-Login/src/app.js
@@ -24,7 +24,6 @@ const environment = () => {
     mongoose.connect(process.env.MONGO_URL)
         .then(() => console.log("Conectado a la base de datos"))
         .catch((error) => console.log("Error al conectar a la base de datos", error));
-
 }
 
 environment();
